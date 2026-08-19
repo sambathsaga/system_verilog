a0308f9dc615b81116e2e7927eaa1f812b88a8c9
SAMBATHKUMAR S <131866247+sambathsaga@users.noreply.github.com>
2026-08-19 18:23:40 +0530

    Create design.sv

---
+++ Fulladder_verification/design.sv
@@ -0,0 +1,10 @@
+module fa(
+  input logic a,
+  input logic b,
+  input logic cin,
+  output logic sum,
+  output logic cout
+);
+  assign sum=a^b^cin;
+  assign cout=((a&b)|(b&cin)|(cin&a));
+endmodule
