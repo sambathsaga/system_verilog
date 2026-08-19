bc235824f89754fa12d2fd3f4ce13384b5bd6e92
SAMBATHKUMAR S <131866247+sambathsaga@users.noreply.github.com>
2026-08-19 18:14:20 +0530

    Delete Dff_verification directory

--- Dff_verification/interface.sv
+++
@@ -1,7 +0,0 @@
-interface fa_intf();
-  logic a;
-  logic b;
-  logic cin;
-  logic sum;
-  logic cout;
-endinterface
--- Dff_verification/transaction.sv
+++
@@ -1,7 +0,0 @@
-class transaction;
-  rand bit a;
-  rand bit b;
-  rand bit cin;
-  bit sum;
-  bit cout;
-endclass
